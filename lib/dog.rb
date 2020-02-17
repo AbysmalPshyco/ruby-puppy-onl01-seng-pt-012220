85050edd606cc8de656244cbbe617c704acf373c
Anthony Risch <rischthefisch@gmail.com>
2020-02-17 01:08:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,11 @@ class Dog
   
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     save  
+=======
+    @@all << self
+>>>>>>> dda997757eaf450f3f94b8ce12dbbd84ddab390f
   end
   def self.all
     @@all
@@ -19,10 +23,17 @@ class Dog
     @@all.each do |dog|
       puts dog.name
   end
+<<<<<<< HEAD
 
 end
 
  def save
    @@all << self
+=======
+end
+
+ def save
+   @@all << save
+>>>>>>> dda997757eaf450f3f94b8ce12dbbd84ddab390f
  end
 end
